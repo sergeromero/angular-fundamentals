e7350291c9b2018580c18fb8e94d14e9d14b0672
Sergio Romero <sergeromero@gmail.com>
2017-12-29 23:00:16 -0500

    Module lazy loading example

--- app/routes.ts
+++ app/routes.ts
@@ -11,5 +11,6 @@ export const appRoutes: Routes = [
     { path: 'events', component: EventsListComponent, resolve: {events : EventsListResolverService} },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivatorService] },
     { path: '404', component: Error404Component },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { path: 'user', loadChildren: 'app/user/user.module#UserModule' }
 ];
---
+++ app/user/user.routes.ts
@@ -0,0 +1,5 @@
+import { ProfileComponent } from './profile.component';
+
+export const userRoutes = [
+    {path: 'profile', component: ProfileComponent} // the route in fact is /user/profile
+];
