e81cd4210a6d9047c1831216f1699fdf68663ce7
Sergio Romero <sergeromero@gmail.com>
2017-12-29 23:18:12 -0500

    Organize imports with barrels example

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,20 +3,23 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivatorService,
+    EventsListResolverService
+} from './events/index';
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/nav.component';
 
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event-component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivatorService } from './events/event-details/event-route-activator.service';
-import { EventsListResolverService } from './events/events-list-resolver.service';
 
 
 @NgModule({
--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
-import { EventsListComponent } from "./events/events-list.component";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
-import { CreateEventComponent } from './events/create-event-component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivatorService } from './events/event-details/event-route-activator.service';
-import { EventsListResolverService } from './events/events-list-resolver.service';
+
+import {
+    EventsListComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivatorService,
+    EventsListResolverService
+} from './events/index';
 
 export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
