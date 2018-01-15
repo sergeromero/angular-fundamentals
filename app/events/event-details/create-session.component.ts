f7e653f6b56031b09459dd1e4a6ff8b0cc1babe9
sromero <sergeromero@users.noreply.github.com>
2018-01-15 16:28:44 -0500

    Custome reusable validator example

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ISession } from '../shared/index';
+import { ISession, restrictedWords } from '../shared/index';
 
 @Component({
     selector: 'selector-name',
@@ -23,7 +23,7 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords(['foo', 'bar'])]);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -47,17 +47,4 @@ export class CreateSessionComponent implements OnInit {
          console.log(session);
          console.log(this.abstract);
      };
-
-     private restrictedWords(words) {
-        return (control: FormControl): {[key: string]: any} => {
-            if(!words) return null;
-
-            let invalidWords = words.map(w => control.value.includes(w) ? w : null)
-                .filter(w => w != null);
-
-            return invalidWords && invalidWords.length > 0
-                ? {'restrictedWords': invalidWords.join(', ')}
-                : null;
-        };
-    };
 };
