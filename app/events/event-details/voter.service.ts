0c83afee0e0625d8a700816fcc1457b8d7c567e0
sromero <sergeromero@users.noreply.github.com>
2018-03-21 10:37:51 -0400

    Finished moving all of the functionality of event requests to the server. The application no longer has any list of events in the client.

--- app/events/event-details/voter.service.ts
+++ app/events/event-details/voter.service.ts
@@ -1,20 +1,38 @@
 import { Injectable } from '@angular/core';
 import { ISession } from '../shared/index';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class VoterService {
+    private urlBase: string = 'http://localhost:8015';
 
-    constructor() { }
+    constructor(private http: Http) { }
 
-    deleteVoter(session: ISession, voterName: string): void {
+    deleteVoter(eventId: number, session: ISession, voterName: string): void {
         session.voters = session.voters.filter(voter => voter !== voterName);
+        
+        let url = `${this.urlBase}/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+
+        this.http.delete(url).catch(this.handleError).subscribe();
     };
 
-    addVoter(session: ISession, voterName: string): void {
+    addVoter(eventId: number, session: ISession, voterName: string): void {
         session.voters.push(voterName);
+
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+
+        let url = `${this.urlBase}/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+
+        this.http.post(url, JSON.stringify({}), options).catch(this.handleError).subscribe();
     };
 
     userHasVoted(session: ISession, voterName: string): boolean{
         return session.voters.some(voter => voter === voterName);
     };
+
+    private handleError(error: Response){
+        return Observable.throw(error.statusText);
+      };
 };
