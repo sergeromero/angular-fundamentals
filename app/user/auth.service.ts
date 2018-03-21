f4449c97537fc91a588888f82320775afc6e644b
sromero <sergeromero@users.noreply.github.com>
2018-03-21 16:26:30 -0400

    Removed the login functionality from the client and moved it to the backend. Missing updating the the user's first and last name to the backend.

--- app/user/auth.service.ts
+++ app/user/auth.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user.model';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class AuthService {
+    private urlBase: string = 'http://localhost:8015';
     currentUser: IUser
 
-    constructor() { }
+    constructor(private http: Http) { }
 
     loginUser(userName: string, password: string){
-        this.currentUser = {
-            id: 1,
-            firstName: 'Sergio',
-            lastName: 'Romero',
-            userName: 'Serge'
-        };
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers});
+
+        let loginInfo = { username: userName, password: password };
+
+        return this.http.post(`${this.urlBase}/api/login`, JSON.stringify(loginInfo), options)
+            .do(resp => {
+                if(resp){
+                    this.currentUser = <IUser>resp.json().user;
+                }
+            }).catch(error => {
+                return Observable.of(false);
+            });
     };
 
     isAuthenticated(): boolean{
--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -7,13 +7,21 @@ import { Router } from '@angular/router';
     styleUrls: ['app/user/login.component.css']
 })
 export class LoginComponent implements OnInit {
+    loginInvalid = false;
+
     constructor(private authService: AuthService, private router: Router) { }
 
     ngOnInit() { }
 
     login(formValues){
-        this.authService.loginUser(formValues.userName, formValues.password);
-        this.router.navigate(['events']);
+        this.authService.loginUser(formValues.userName, formValues.password).subscribe(resp => {
+            if(!resp){
+                this.loginInvalid = true;
+            }
+            else{
+                this.router.navigate(['events']);
+            }
+        });        
     };
 
     cancel(){
